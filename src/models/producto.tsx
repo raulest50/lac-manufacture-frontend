86b4fdeb428fe2e148f9e4baecfaa236c361861c
esteban <raulest50@gmail.com>
2024-07-14 23:02:49 -0500

    se actualizo el codigo fuente en la secciond de codificar producto

--- src/models/producto.tsx
+++ src/models/producto.tsx
@@ -3,6 +3,6 @@
 
 const TIPOS_PRODUCTOS = {materiaPrima: "M", semiTerminado:"S", Terminado:"T"};
 
-const UNIDADES = {KG:'KG', L:'L', U:'U'};
+const UNIDADES = {L:"L", KG:"KG", U:"U"};
 
 export {TIPOS_PRODUCTOS, UNIDADES}
--- src/pages/Producto/CrearProducto.tsx
+++ src/pages/Producto/CrearProducto.tsx
@@ -14,11 +14,15 @@ import {
     Textarea, Heading, Text,
     List, ListItem,
     Card, CardBody, CardHeader,
-    Select, Icon,
+    Select, Icon, IconButton,
 } from '@chakra-ui/react'
 
 import { IoCubeSharp } from "react-icons/io5";
 import { FaCubes } from "react-icons/fa";
+
+import { MdWaterDrop } from "react-icons/md"; // L
+import { FaHashtag } from "react-icons/fa6"; // U
+import { GiWeight } from "react-icons/gi";  // KG
 
 // Supports weights 100-900
 import '@fontsource-variable/league-spartan';
@@ -69,21 +73,40 @@ function CrearProducto(){
     const [costo, setCosto] = useState('');
     const [observaciones, setObservaciones] = useState('');
     const [tipo_unidad, setTipo_unidad] = useState(UNIDADES.KG);
-    const [cantidad_unidad, setCantidad_unidad] = useState(0);
+    const [cantidad_unidad, setCantidad_unidad] = useState('');
+
+    const TIPO_BUSQUEDA = {NOMBRE:"NOMBRE", ID:"ID"}
+
+    const [busqueda, setBusqueda] = useState('');
+    const [busqueda_tipo_mp, setBusqueda_tipo_mp] = useState(true)
+    const [busqueda_param, setBusqueda_param] = useState(TIPO_BUSQUEDA.NOMBRE)
     //const [tipo_producto, setTipo_producto] = useState(TIPOS_PRODUCTOS.semiTerminado)
 
     const [listaProductos, setListaProductos] = useState([])
 
     type MiItem = {
-        id: number;
+        producto_id: number;
         nombre: string;
         observaciones: string;
         costo: number;
-        tipo_producto: string;
+        tipo_unidades: string;
         fechaCreacion: string;
     };
 
     const toast = useToast()
+
+    const get_UnitsIcon = (tipo_unidades:string) => {
+        switch (tipo_unidades) {
+            case 'L':
+                return MdWaterDrop;
+            case 'KG':
+                return GiWeight;
+            case 'U':
+                return FaHashtag;
+            default:
+                return FaHashtag;
+        }
+    };
 
     const saveMateriaPrimSubmit = async () => {
         const materia_prima = {
@@ -193,8 +216,13 @@ function CrearProducto(){
                                         <FormControl flex={"4"}>
                                             <FormLabel>Cantidad por Unidad</FormLabel>
                                             <Input
+                                                //inputMode={"decimal"}
+                                                //pattern={"[0-9]*[.]?[0-9]*"}
                                                 value={cantidad_unidad}
-                                                onChange={(e) => setCantidad_unidad(Number(e.target.value))}
+                                                onChange={(e) => {
+                                                        setCantidad_unidad(e.target.value)
+                                                    }
+                                                }
                                                 variant={'filled'}/>
                                         </FormControl>
                                     </Flex>
@@ -211,26 +239,56 @@ function CrearProducto(){
                         <HStack p={0} m={0} w={'full'} h={'full'}>
 
                             {/*panel izquierdo*/}
-                            <VStack w={'full'} h={'full'}>
-                                <Heading p={2} bg={'green.200'} size={'md'}>Productos Existentes</Heading>
-                                <Box w={'full'} h={'full'}>
+                            <VStack w={'full'} >
+                                <HStack w={'full'} h={'full'}>
+                                    <Heading p={2} bg={'green.200'} size={'md'}>Bandeja de Seleccion</Heading>
+                                    <Button
+                                        onClick={getProductosTodos}
+                                        colorScheme={'teal'}>Cargar Productos</Button>
+                                </HStack>
+                                <HStack w={'full'} alignItems={"center"}>
+                                    <FormControl>
+                                        <FormLabel>Buscar</FormLabel>
+                                        <Input
+                                            value={busqueda}
+                                            onChange={(e) => setBusqueda(e.target.value)}
+                                            sx={input_style}/>
+                                    </FormControl>
+                                    <IconButton
+                                        aria-label='Search database'
+                                        icon={busqueda_tipo_mp ? <IoCubeSharp/> : <FaCubes/>}
+                                        onClick={ () =>{
+                                            setBusqueda_tipo_mp(!busqueda_tipo_mp);
+                                        }}
+                                        fontSize={{ base: "1.2em", md: "2em", lg: "2.8m", xl:"3.5em" }}  // Responsive font size
+                                        size={"lg"}
+                                    />
+                                    <Select defaultValue={TIPO_BUSQUEDA.NOMBRE}
+                                            value={busqueda_param}
+                                            onChange={(e) => setBusqueda_param(e.target.value)}
+                                    >
+                                        <option value={TIPO_BUSQUEDA.NOMBRE}>{TIPO_BUSQUEDA.NOMBRE}</option>
+                                        <option value={TIPO_BUSQUEDA.ID}>{TIPO_BUSQUEDA.ID}</option>
+                                    </Select>
+                                </HStack>
+                                <Box w={'full'} >
                                     <List spacing={'0.5em'}>
                                         {listaProductos.map((item:MiItem) => (
-                                            <ListItem key={item.id}>
+                                            <ListItem key={item.producto_id}>
                                                 <Box>
                                                     <Card sx={cardItem_style} fontFamily={'Comfortaa Variable'} p={'0.7em'}>
                                                         <CardHeader p={1} >
                                                             <HStack justifyContent={'space-evenly'} >
                                                                 <Heading size={'sm'} fontFamily={'Anton'} fontSize={'1.2em'}> {item.nombre} </Heading>
-                                                                <Heading size={'sm'} fontFamily={'Anton'} fontSize={'1.2em'}> Id: {item.id} </Heading>
+                                                                <Heading size={'sm'} fontFamily={'Anton'} fontSize={'1.2em'}> Id: {item.producto_id} </Heading>
                                                             </HStack>
                                                         </CardHeader>
                                                         <CardBody p={1}>
                                                             <HStack p={'1em'}>
-                                                                <Icon boxSize={'3em'} mr={'1em'} as={item.tipo_producto === 'Materia Prima' ? IoCubeSharp : FaCubes}/>
+                                                                <Icon boxSize={'3em'} mr={'1em'} as={get_UnitsIcon(item.tipo_unidades)}/>
                                                                 <VStack justifyContent={'space-evenly'} alignItems={'start'} pl={'1em'}>
                                                                     <Text>Costo: {item.costo}</Text>
-                                                                    <Text>Tipo: {item.tipo_producto}</Text>
+                                                                    <Text>Tipo: {item.tipo_unidades}</Text>
                                                                     <Text>Fecha Creacion: {new Date(item.fechaCreacion).toLocaleString()}</Text>
                                                                 </VStack>
 
@@ -242,14 +300,11 @@ function CrearProducto(){
                                         ))}
                                     </List>
                                 </Box>
-                                <Button
-                                    onClick={getProductosTodos}
-                                    colorScheme={'teal'}>Cargar Productos</Button>
                             </VStack>
 
 
                             {/*panel derecho*/}
-                            <VStack w={'full'} h={'full'}>
+                            <VStack w={'full'} h={'full'} alignItems="flex-start">
                                 <FormControl>
                                     <Select defaultValue={TIPOS_PRODUCTOS.semiTerminado}>
                                         <option value={1}>{TIPOS_PRODUCTOS.semiTerminado}</option>
